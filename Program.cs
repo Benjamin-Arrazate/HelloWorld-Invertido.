a742bdde8db12ebdea0433ab83df0877dd494780
Benjamín Arrazate <benjaminarrazate@MacBook-Pro-de-Benjamin.local>
2021-02-14 20:25:25 -0700

    Hello World Inverso

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ARRAY_BIDI_ESMIONAÑ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "HelloWorld!";
+            char[] charArray = texto.ToCharArray();
+            Array.Reverse( charArray );
+            Console.WriteLine(new string(charArray));
+            
+        }
+    }
+}
